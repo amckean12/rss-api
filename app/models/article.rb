d776abb662574d9580b38d73488a55ef82df85c3
amckean12 <amckean@me.com>
2020-03-29 14:56:37 -0500

    Passes article validation tests

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,10 @@
 class Article < ApplicationRecord
+    #Model Vaidations
+    validates :title, presence: true
+    validates :article_link, presence: true
+    validates :published_date, presence: true
+    validates :description, presence: true
+    validates :image_present, presence: true
+    validates :title, length: { maximum: 50 }
+    validates :description, length: { maximum: 150 }
 end
