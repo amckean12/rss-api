e9cf0a0b2cdf4356dd00b33bc6019e0d0b5bda4f
amckean12 <amckean@me.com>
2020-03-29 14:24:42 -0500

    Stubs out initial test cases

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,5 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe Article, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  #Ensure presence of all necessary article attributes
+  it "is not valid without a title"
+  it "is not valid without an article link"
+  it "is not valid without a published date"
+  it "is not valid witout a description"
+  it "is not valid without an image_present boolean"
+
+  #Ensures each article attribute adhears to the formating rules
+  context "is not valid unless title formatting of" do 
+    it "is a string"
+    it "maximum 50 characters is met"
+    it "contains only letters and spaces is met"
+  end
+  context "is not valid unless article link formatting of" do
+    it "is a string"
+    it "containes a full redirectable link"
+  end
+  context "is not valid unless article published date formatting of" do 
+    it "is a correctly formatted datetime"
+  end
+  context "is not valid unless article description formatting of" do
+    it "is a text value"
+    it "has a maximum character count of 150 characters"
+    it "only containes letters and spaces"
+  end
+  context "is not valid unless article image_true formatting of" do 
+    it "is a boolean value"
+  end
 end
