80819aa348c9c6510729d49dd640527a087f7ca8
amckean12 <amckean@me.com>
2020-03-29 11:50:33 -0500

    Adds user controller route models

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
-
+    #Tests to ensure correct routing for User
+    it "should route => GET: users/1 to the users controller,  show action, user id of 1" do
+        expect(GET: "/users/1").to route_to(controller: 'users', action: 'show', id:'1') 
+    end
+    it "should route => PUT: users/1 to the users contorller, update action, user id of 1" do 
+        expect(PUT: "/users/1").to route_to(controller: 'users', action: 'update', id:'1') 
+    end
+    it "should route => GET: users to the users controller, index action" do 
+        expect(GET: "/users").to route_to(controller: 'users', action: 'index') 
+    end
+    it "should route => DELETE: user/1 to the users controller, delete action, user id of 1" do 
+        expect(DELETE: "/users/1").to route_to(controller: 'users', action: 'delete', id:'1') 
+    end
 end
