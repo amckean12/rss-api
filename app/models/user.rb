66d0efa62e72d0cd8826bacb7b32121ae42ce399
amckean12 <amckean@me.com>
2020-03-29 11:17:08 -0500

    finishes user model validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,12 @@
 class User < ApplicationRecord
-    #Use Model Validations
-    validates :username, presence: true
-    validates :username, uniqueness: true
-    validates :username, format: { 
-        with: /\A[a-zA-Z0-9]+\z/,
-        message: "username only allows letters and numbers"
-    }
-    validates :username, length: { maximum: 32 }
+  has_secure_password
+
+  #Model Validations
+  validates :username, presence: true
+  validates :username, uniqueness: true
+  validates :username, format: { 
+      with: /\A[a-zA-Z0-9]+\z/,
+      message: "username only allows letters and numbers"
+  }
+  validates :username, length: { maximum: 32 }
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -25,22 +25,7 @@ RSpec.describe User, type: :model do
   it 'is not valid without password' do 
     should validate_presence_of(:password).on(:create)
   end
-  it 'is not valid without password length greater than 5 characters' do 
-    should ensure_length_of(:password).is_at_least(6)
-  end
-  it 'is not valid without password length less than 26 characters' do
-    should ensure_length_of(:password).is_at_most(25)
-  end
-  it 'is not valid if password does not include special character' do
-    should_not allow_values('ajmckean', 'ajmckean5', 'Ajmckean5').for(:password)
-  end
-  it 'is not valid if password includes spaces' do 
-    should_not allow_values('alex mckean', '@lex McKean5', 'alexmckean 5').for(:password)
-  end
-  it 'is not valid if password doesnt include lower case' do 
-    should_not allow_values('@LEXMCKEAN4').for(:password)
-  end
-  it 'is not valid if password does not include capital letter, special character, and number' do
-    should allow_values('@lexMckean5').for(:password)
+  it 'is not valid without a secure password' do
+    should have_secure_password 
   end
 end
