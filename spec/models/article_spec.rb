ecbc5655686a3e99242e6082d6eb2c1af7cf61ca
amckean12 <amckean@me.com>
2020-03-29 14:48:40 -0500

    Adds article validation tests

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -2,31 +2,39 @@ require 'rails_helper'
 
 RSpec.describe Article, type: :model do
   #Ensure presence of all necessary article attributes
-  it "is not valid without a title"
-  it "is not valid without an article link"
-  it "is not valid without a published date"
-  it "is not valid witout a description"
-  it "is not valid without an image_present boolean"
+  it "is not valid without a title" do 
+    should validate_presence_of(:title).on(:create)
+  end
+  it "is not valid without an article link" do
+    should validate_presence_of(:article_link).on(:create) 
+  end
+  it "is not valid without a published date" do
+    should validate_presence_of(:published_date).on(:create)
+  end
+  it "is not valid witout a description" do
+    should validate_presence_of(:description).on(:create) 
+  end
+  it "is not valid without an image_present boolean" do
+    should validate_presence_of(:image_present).on(:create) 
+  end
 
   #Ensures each article attribute adhears to the formating rules
   context "is not valid unless title formatting of" do 
-    it "is a string"
-    it "maximum 50 characters is met"
-    it "contains only letters and spaces is met"
+    it "maximum 50 characters is met" do 
+      should validate_length_of(:title).is_at_most(50)
+    end
   end
   context "is not valid unless article link formatting of" do
-    it "is a string"
-    it "containes a full redirectable link"
+    it "containes a full redirectable link" do 
+    end
   end
   context "is not valid unless article published date formatting of" do 
-    it "is a correctly formatted datetime"
   end
   context "is not valid unless article description formatting of" do
-    it "is a text value"
-    it "has a maximum character count of 150 characters"
-    it "only containes letters and spaces"
+    it "has a maximum character count of 150 characters" do
+      should validate_length_of(:description).is_at_most(150) 
+    end
   end
   context "is not valid unless article image_true formatting of" do 
-    it "is a boolean value"
   end
 end
