af7f41db4c700e6e7a193e8d6bbf5ac76e601d65
amckean12 <amckean@me.com>
2020-03-30 16:53:49 -0500

    cleans up unused code

--- app/serializers/article_serializer.rb
+++
@@ -1,3 +0,0 @@
-class ArticleSerializer < ActiveModel::Serializer
-    attributes :id, :title, :article_link, :published_date, :description, :image, :img_src, :img_alt
-end
--- spec/controllers/articles_controller_spec.rb
+++ spec/controllers/articles_controller_spec.rb
@@ -1,8 +1,4 @@
 require 'rails_helper'
 
 RSpec.describe ArticlesController, type: :controller do
-    #Tests to ensure correct routing for User
-    it "should route => GET: articles to the articles controller, index action" do 
-        expect(GET: "/articles").to route_to(controller: 'articles', action: 'index') 
-    end
 end
